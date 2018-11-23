63d0327fbd78300b8fd4b7dcc841498bae3e73a3
theLittleBigZ <alt340760115@gmail.com>
2018-11-23 17:03:08 -0500

    bug fixed
    working now

--- palindromes.py
+++ palindromes.py
@@ -49,7 +49,7 @@ def count_palindromes(input_file):
         line = line.rstrip()
         # Checks if each line is a palindrome
         if is_palindrome(line):
-            total_palindromes = 0
+            total_palindromes = total_palindromes + 1#here the error
     return total_palindromes
 
 
