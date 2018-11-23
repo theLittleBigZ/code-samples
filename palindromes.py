cb11d6be8ebbaa44736b7ef5166b11df836d074f
theLittleBigZ <alt340760115@gmail.com>
2018-11-23 17:12:24 -0500

    Update palindromes.py

--- palindromes.py
+++ palindromes.py
@@ -49,7 +49,7 @@ def count_palindromes(input_file):
         line = line.rstrip()
         # Checks if each line is a palindrome
         if is_palindrome(line):
-            total_palindromes = total_palindromes + 1 #changed mkiii
+            total_palindromes = total_palindromes + 1 #changed mkiiii
     return total_palindromes
 
 
